d0196eb7b442b46fb2367f677060fb460af50397
Nico <nico.preen@gmx.de>
2018-12-10 14:03:47 +0100

    removed useless plugins and comments

--- src/main/java/de/jnd/App.java
+++ src/main/java/de/jnd/App.java
@@ -3,15 +3,12 @@ package de.jnd;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        Arrays.stream(args).filter(c -> c.startsWith("-")).collect(Collectors.toList()).stream().forEach(System.out::println);
+        Arrays.stream(args).filter(c -> c.startsWith("-")).collect(Collectors.toList()).forEach(System.out::println);
 
     }
 }
