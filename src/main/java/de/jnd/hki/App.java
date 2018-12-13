bfadf58cec0b62d8a26f13489b224f6db5c796a8
Nico <nico.preen@gmx.de>
2018-12-13 13:08:41 +0100

    added logger and deep learning 4j example

--- src/main/java/de/jnd/hki/App.java
+++ src/main/java/de/jnd/hki/App.java
@@ -1,10 +1,28 @@
 package de.jnd.hki;
 
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.nd4j.linalg.learning.config.Sgd;
+
 public class App {
 
 
     public static void main(String[] args) {
 
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .weightInit(WeightInit.XAVIER)
+                .activation("relu")
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .updater(new Sgd(0.05))
+                // ... other hyperparameters
+                .list()
+                .backprop(true)
+                .layer(0, new DenseLayer.Builder().nIn(784).nOut(250)
+                        .build())
+                .build();
 
     }
 }
