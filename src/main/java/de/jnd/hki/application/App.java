99ef42763cb1df67e915a0eb11b5c0e39ffbcd31
Test User <test@mail.com>
2019-01-29 08:55:51 +0100

    Added a cli example for gui and console changed maven/jar structure to load dependencies to the jar

--- src/main/java/de/jnd/hki/application/App.java
+++ src/main/java/de/jnd/hki/application/App.java
@@ -1,9 +1,25 @@
 package de.jnd.hki.application;
 
+import de.jnd.hki.controller.BaseUtils;
+
+import java.util.Map;
+
 public class App {
 
+	//mode=gui,console(default)
+	public static void main(String[] args) {
+		Map<String, String> argsMap = BaseUtils.convertArgsToMap(args);
 
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
+		switch (argsMap.get("mode") == null ? "" : argsMap.get("mode")) {
+		case "gui":
+			Gui.main(null);
+			break;
+		case "console":
+			break;
+		default:
+			System.out.println("default");
+			break;
+		}
+	}
+
 }
