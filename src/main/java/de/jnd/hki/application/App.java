5d13efa13e6b9da16f481cb5d631abf6b4fc72e2
Nico <nico.preen@gmx.de>
2019-02-11 08:38:53 +0100

    cli

--- src/main/java/de/jnd/hki/application/App.java
+++ src/main/java/de/jnd/hki/application/App.java
@@ -1,7 +1,7 @@
 package de.jnd.hki.application;
 
+import de.jnd.hki.controller.BaseUtils;
 import de.jnd.hki.controller.NetworkController;
-import org.datavec.image.loader.NativeImageLoader;
 import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
 import org.deeplearning4j.eval.Evaluation;
 import org.deeplearning4j.nn.conf.InputPreProcessor;
@@ -9,72 +9,68 @@ import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.preprocessor.CnnToFeedForwardPreProcessor;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
-import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
-import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class App {
     private static Logger log = LoggerFactory.getLogger(App.class);
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        int height = 28;
-        int width = 28;
-        int channels = 1;
-
-        // recordReader.getLabels()
-        // In this version Labels are always in order
-        // So this is no longer needed
-        //List<Integer> labelList = Arrays.asList(2,3,7,1,6,4,0,5,8,9);
-        ArrayList<Integer> labelList = new ArrayList<Integer>() {{
-            add(0);
-            add(1);
-            add(2);
-            add(3);
-            add(4);
-            add(5);
-            add(6);
-            add(7);
-            add(8);
-            add(9);
-        }};
-
-        MultiLayerNetwork model = NetworkController.loadNetwork("networks/model.zip");
-        NativeImageLoader loader = new NativeImageLoader(height, width, channels);
-
-        while (true) {
-            String filechose = fileChose().toString();
-            File file = new File(filechose);
-
-            if(file == null)
-                System.exit(0);
-
-            INDArray image = loader.asMatrix(file);
-            DataNormalization scaler = new ImagePreProcessingScaler(0, 1);
-            scaler.transform(image);
-
-            INDArray output = model.output(image);
-            float best = 0;
-            int bestIndex = 0;
-
-            for(int i = 0;i<10;i++){
-                float val = output.data().asFloat()[i];
-                if(best < val){
-                    best = val;
-                    bestIndex = i;
-                }
-            }
-            System.out.println(labelList.get(bestIndex));
-        }
-    }
+//    public static void main(String[] args) throws IOException, InterruptedException {
+//        int height = 28;
+//        int width = 28;
+//        int channels = 1;
+//
+//        // recordReader.getLabels()
+//        // In this version Labels are always in order
+//        // So this is no longer needed
+//        //List<Integer> labelList = Arrays.asList(2,3,7,1,6,4,0,5,8,9);
+//        ArrayList<Integer> labelList = new ArrayList<Integer>() {{
+//            add(0);
+//            add(1);
+//            add(2);
+//            add(3);
+//            add(4);
+//            add(5);
+//            add(6);
+//            add(7);
+//            add(8);
+//            add(9);
+//        }};
+//
+//        MultiLayerNetwork model = NetworkController.loadNetwork("networks/model.zip");
+//        NativeImageLoader loader = new NativeImageLoader(height, width, channels);
+//
+//        while (true) {
+//            String filechose = fileChose().toString();
+//            File file = new File(filechose);
+//
+//            if(file == null)
+//                System.exit(0);
+//
+//            INDArray image = loader.asMatrix(file);
+//            DataNormalization scaler = new ImagePreProcessingScaler(0, 1);
+//            scaler.transform(image);
+//
+//            INDArray output = model.output(image);
+//            float best = 0;
+//            int bestIndex = 0;
+//
+//            for(int i = 0;i<10;i++){
+//                float val = output.data().asFloat()[i];
+//                if(best < val){
+//                    best = val;
+//                    bestIndex = i;
+//                }
+//            }
+//            System.out.println(labelList.get(bestIndex));
+//        }
+//    }
 
     public static String fileChose() {
         JFileChooser fc = new JFileChooser();
@@ -129,5 +125,22 @@ public class App {
 
         NetworkController.saveNetwork(model, "networks/model.zip", true);
         return model;
+    }
+
+
+    //mode=gui,console(default)
+    public static void main(String[] args) {
+        Map<String, String> argsMap = BaseUtils.convertArgsToMap(args);
+
+        switch (argsMap.get("mode") == null ? "" : argsMap.get("mode")) {
+            case "gui":
+                Gui.main(null);
+                break;
+            case "console":
+                break;
+            default:
+                System.out.println("default");
+                break;
+        }
     }
 }
