35a28da9bc8618f6c2423b97b1c41a003659bb44
Jannik Ahlers <ahlers@univention.de>
2019-02-15 13:06:48 +0100

    opencv test

--- src/main/java/de/jnd/hki/application/App.java
+++ src/main/java/de/jnd/hki/application/App.java
@@ -1,10 +1,20 @@
 package de.jnd.hki.application;
 
+import de.jnd.hki.testCV;
+import org.bytedeco.javacpp.Loader;
+import org.bytedeco.javacpp.opencv_java;
+
 public class App {
 
 
     public static void main(String[] args) {
-        nu.pattern.OpenCV.loadShared();
-        testCV.test();
+        //nu.pattern.OpenCV.loadShared();
+        Loader.load(opencv_java.class);
+        //testCV.testAffineTransformation();
+        //testCV.detectEdges(testCV.imgImport());
+        //testCV.imgExport(testCV.imgImport(), "testkreuzbin.png");
+        //testCV.imgExport(testCV.detectCross(testCV.imgImport()), "detectcross.png");
+        //testCV.imgExport(testCV.detectCrossCascade(testCV.imgImport()), "detectcross.png");
+        testCV.imgExport(testCV.detectAruCo(testCV.imgImport()), "detectaruco.png");
     }
 }
--- src/main/java/de/jnd/hki/testCV.java
+++ src/main/java/de/jnd/hki/testCV.java
@@ -1,14 +1,19 @@
 package de.jnd.hki;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfPoint2f;
-import org.opencv.core.Point;
-import org.opencv.core.Size;
+import org.bytedeco.javacpp.opencv_dnn;
+import org.opencv.aruco.Dictionary;
+import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.aruco.Aruco;
+
+import java.util.ArrayList;
+//import java.util.Dictionary;
+import java.util.List;
 
 public class testCV {
-    public static void test() {
-        String file = "/home/jahlers/schule/mittelstufenprojekt/papier.jpg";
+    public static void testAffineTransformation() {
+        String file = "/home/jahlers/schule/msp/papier.jpg";
         Mat src = Imgcodecs.imread(file);
         Mat dst = new Mat();
 
@@ -28,7 +33,103 @@ public class testCV {
         Size size = new Size(src.cols(), src.rows());
         Imgproc.warpAffine(src, dst, transformMatrix, size);
 
-        Imgcodecs.imwrite("/home/jahlers/schule/mittelstufenprojekt/papier_affine.jpg", dst);
+        Imgcodecs.imwrite("/home/jahlers/schule/msp/papier_affine.jpg", dst);
         System.out.println("Fertig!");
+    }
+
+    public static Mat imgImport() {
+        Imgcodecs imageCodecs = new Imgcodecs();
+        //String file = "/home/jahlers/schule/msp/images/echtespapier.jpg";
+        //String file = "/home/jahlers/schule/msp/images/template_v1.png";
+        String file = "/home/jahlers/schule/msp/images/template_v3.png";
+
+        Mat src = new Mat();
+        src = imageCodecs.imread(file, Imgcodecs.IMREAD_GRAYSCALE);
+        Mat dst = new Mat();
+        Imgproc.threshold(src, dst, 170, 255, Imgproc.THRESH_BINARY);
+        Mat dst2 = new Mat();
+        Imgproc.pyrDown(dst,dst2);
+        System.out.println("Image loaded.");
+        return dst2;
+    }
+
+    public static void imgExport(Mat src, String filename) {
+        Imgcodecs imageCodecs = new Imgcodecs();
+        imageCodecs.imwrite("/home/jahlers/schule/msp/images/" + filename, src);
+        System.out.println("Image saved.");
+    }
+
+    public static void detectEdges(Mat mat) {
+        List<MatOfPoint> contours = new ArrayList();
+        Mat edges = new Mat();
+        Mat dest = Mat.zeros(mat.size(), CvType.CV_8UC3);
+        Scalar black = new Scalar(0, 0, 0);
+        Scalar white = new Scalar(255, 255, 255);
+        //Imgproc.findContours(mat, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_TC89_KCOS);
+        //Imgproc.drawContours(dest, contours, -1, white);
+        Imgproc.Canny(mat, edges, 60., 60.*3, 5, false);
+        imgExport(edges, "edges.png");
+    }
+
+    public static Mat detectCross(Mat src) {
+        MatOfPoint corners = new MatOfPoint();
+        Mat dest = new Mat();
+        Imgproc.pyrDown(src, dest);
+        Imgproc.goodFeaturesToTrack(dest, corners, 50, 0.70, 100.);
+        for (Point corner: corners.toArray()) {
+            corner.x = corner.x*2;
+            corner.y = corner.y*2;
+            Imgproc.circle(src, corner, 6, new Scalar(0, 0, 255), 2);
+        }
+        return src;
+    }
+
+    public static Mat detectCrossCascade(Mat mat) {
+        String xmlFile = "/home/jahlers/schule/msp/images/training/cascade/cascade.xml";
+        CascadeClassifier classifier = new CascadeClassifier(xmlFile);
+        MatOfRect crossDetections = new MatOfRect();
+        classifier.detectMultiScale(mat, crossDetections);
+        System.out.println(xmlFile);
+        System.out.println(String.format("Detected %s crosses", crossDetections.toArray().length));
+
+        for (Rect rect : crossDetections.toArray()) {
+            Imgproc.rectangle(
+                mat,                                                        // where to draw the box
+                new Point(rect.x, rect.y),                                  // bottom left
+                new Point(rect.x + rect.width, rect.y + rect.height), // top right
+                new Scalar(0, 0, 255),
+                3                                                  // RGB colour
+            );
+        }
+
+        return mat;
+    }
+
+    public static List<Point> detectAruCo(Mat src) {
+        List<Mat> markers = new ArrayList<>();
+        Mat ids = new Mat();
+        Dictionary dictionary = Aruco.getPredefinedDictionary(Aruco.DICT_4X4_50);
+        Aruco.detectMarkers(src, dictionary, markers, ids);
+        List<Point> centers = new ArrayList<>();
+        for (Mat marker: markers) {
+            /*System.out.println(marker.get(0,0)[0]);
+            Point cornerTopLeft =  new Point(marker.get(0,0));
+            Point cornerBottomRight = new Point(marker.get(0,2));
+            Imgproc.rectangle(
+                    src,
+                    cornerTopLeft,
+                    cornerBottomRight,
+                    new Scalar(0,0,255),
+                    3
+            );*/
+            Point a = new Point(marker.get(0,0));
+            Point b = new Point(marker.get(0,1));
+            Point c = new Point(marker.get(0,2));
+            Point d = new Point(marker.get(0,3));
+            double x = 0.25 * (a.x + b.x + c.x +d.x);
+            double y = 0.25 * (a.y + b.y + c.y +d.y);
+            centers.add(new Point(x,y));
+        }
+        return centers;
     }
 }
