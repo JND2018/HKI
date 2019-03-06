cea7ba2f01c2b1b19a193de16065c1399e00716a
Daniel <d.steinke_98@web.de>
2019-03-06 10:10:11 +0100

    Draw Panel

--- src/main/java/de/jnd/hki/controller/DrawPanelController.java
+++ src/main/java/de/jnd/hki/controller/DrawPanelController.java
@@ -7,50 +7,56 @@ import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.MoveTo;
-import javafx.scene.shape.Path;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
 import org.apache.log4j.Logger;
 
 
-public class DrawPanelController {
+public class DrawPanelController{
 
-    private Path path;
+    private Path path = null;
     private Group lineGroup;
-    private static final double DEFAULTTHICKNESS = 3.0;
-    private static final double MAXTHICKNESS = 30.0;
-    private static final double MINTHICKNESS = 1.0;
+    private static final double DEFAULTTHICKNESS = 5.0;
+    private static final double MAXTHICKNESS = 20.0;
+    private static final double MINTHICKNESS = 5.0;
 
 
     private static Logger log = Logger.getLogger(DrawPanelController.class);
 
     @FXML
-    private HBox utilBox;
+    private AnchorPane mainWindow;
 
     @FXML
     private Button clearBtn;
 
     @FXML
-    private Slider lineThickness;
+    private Line sampleLine;
 
     @FXML
-    private StackPane stackPane;
+    private Slider lineThickness;
 
     @FXML
     private Rectangle drawPanelCnv;
 
+    @FXML
+    private Group root;
+
+
+    public DrawPanelController() {
+    }
+
+    @FXML
+    public void initialize() {
+        lineGroup = new Group();
+        setDrawPanelCnv(drawPanelCnv);
+        setSampleLine(sampleLine);
+        setLineThickness(lineThickness);
+    }
 
     @FXML
     void clearButtonOnMousePressed(ActionEvent event) {
         lineGroup.getChildren().removeAll(lineGroup.getChildren());
-    }
-
-    //Utilbox
-    public void setUtilBox(HBox utilBox) {
-        utilBox.getChildren().addAll(clearBtn, lineThickness);
     }
 
     //lineThicknessSlider
@@ -60,44 +66,51 @@ public class DrawPanelController {
         lineThickness.setValue(DEFAULTTHICKNESS);
     }
 
-    //lineThickness Anzeige
-    Line sampleLine = new Line(0,0,140,0);
 
     public void setSampleLine(Line sampleLine) {
+        sampleLine.setFill(Color.WHITE);
         sampleLine.strokeWidthProperty().bind(lineThickness.valueProperty());
+        //mainWindow.setPrefHeight(MAXTHICKNESS);
     }
 
-    public void setStackPane(StackPane stackPane) {
-        stackPane.setPrefHeight(MAXTHICKNESS);
-        stackPane.getChildren().addAll(sampleLine);
-    }
+
 
     //Rectangle
     public void setDrawPanelCnv(Rectangle drawPanelCnv) {
         drawPanelCnv.setCursor(Cursor.CROSSHAIR);
-        drawPanelCnv.setX(20);
-        drawPanelCnv.setY(20);
+        drawPanelCnv.setFill(Color.BLACK);
+        //drawPanelCnv.setX(20);
+        //drawPanelCnv.setY(20);
     }
+
 
     @FXML
     void drawPanelOnMouseDragged(MouseEvent event) {
         if(drawPanelCnv.getBoundsInLocal().contains(event.getX(), event.getY())){
-            path.getElements().add(new MoveTo(event.getSceneX(), event.getSceneY()));
+            path.getElements().add(new LineTo(event.getSceneX(), event.getSceneY()));
         }
     }
+
 
     @FXML
     void drawPanelOnMousePressed(MouseEvent event) {
         path = new Path();
-        path.setMouseTransparent(true);
+        path.setVisible(true);
+        path.setStroke(Color.WHITE);
         path.setStrokeWidth(sampleLine.getStrokeWidth());
         path.setStroke(sampleLine.getStroke());
         lineGroup.getChildren().add(path);
         path.getElements().add(new MoveTo(event.getSceneX(), event.getSceneY()));
+        test();
+
     }
 
     @FXML
     void drawPanelOnMouseReleased(MouseEvent event) {
         path = null;
+    }
+
+    void test(){
+        root.getChildren().addAll(drawPanelCnv, lineGroup);
     }
 }
